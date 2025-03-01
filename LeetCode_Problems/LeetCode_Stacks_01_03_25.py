0ce3de837c32df23b8ff98adadbe1d288a65d467
Tehman700 <tehmanfast@gmail.com>
2025-03-01 16:35:01 +0500

    Solved 4. Number Problem LeetCode

--- LeetCode_Problems/LeetCode_Stacks_01_03_25.py
+++ LeetCode_Problems/LeetCode_Stacks_01_03_25.py
@@ -75,31 +75,80 @@ from colorsys import rgb_to_hls
 
 # 80. Remove Duplicates From Sorted Array II
 
+#
+# nums = [0,0,1,1,1,1,2,3,3]
+#
+# left = 0
+# right = 1
+# r = False
+#
+# while(right < len(nums)):
+#
+#     if(r==False):
+#         if(nums[left] == nums[right]):
+#             r = True
+#
+#         left+=1
+#         right+=1
+#
+#
+#
+#     if r ==True:
+#         while(right<len(nums) and nums[left] == nums[right]):
+#             nums.pop(right)
+#         left+=1
+#         right+=1
+#         r=False
+# print(len(nums))
 
-nums = [0,0,1,1,1,1,2,3,3]
-
-left = 0
-right = 1
-r = False
-
-while(right < len(nums)):
-
-    if(r==False):
-        if(nums[left] == nums[right]):
-            r = True
-
-        left+=1
-        right+=1
 
 
 
-    if r ==True:
-        while(right<len(nums) and nums[left] == nums[right]):
-            nums.pop(right)
-        left+=1
-        right+=1
-        r=False
-print(len(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# 4. Median of Two Sorted Arrays
+
+# class Solution(object):
+#     def findMedianSortedArrays(self, nums1, nums2):
+#         """
+#         :type nums1: List[int]
+#         :type nums2: List[int]
+#         :rtype: float
+#         """
+#
+#         # Merge the two lists
+#         nums1.extend(nums2)
+#
+#         # Sort the merged list (O(n log n) instead of O(n²))
+#         nums1.sort()
+#
+#         n = len(nums1)
+#
+#         # Find the median
+#         if n % 2 == 0:  # Even length
+#             ty = n // 2
+#             return (nums1[ty] + nums1[ty - 1]) / 2.0  # Return float
+#
+#         else:  # Odd length
+#             ty = n // 2
+#             return float(nums1[ty])  # Ensure float return type
+#
 
 
 
