d8c1cc915cd9376426fa94138bd04c5b50d3fda8
Tehman700 <tehmanfast@gmail.com>
2025-03-01 15:02:57 +0500

    Now solving Problems Related to Stacks and Arrays

---
+++ LeetCode_Problems/LeetCode_Stacks_01_03_25.py
@@ -0,0 +1,29 @@
+# 2460. Apply Operations to an Array
+
+nums = [1,0,2,0,0,1]
+left = 0
+right = left+1
+t = 0
+
+for i in range(len(nums) -1):
+
+    first = nums[left]
+    sec = nums[right]
+
+    if(first == sec):
+        nums[left] = nums[left] * 2
+        nums[right] = 0
+
+    left+=1
+    right+=1
+print(nums)
+
+for i in nums[:]:
+    if(i == 0):
+        nums.remove(i)
+        t+=1
+
+for _ in range(t):
+    nums.append(0)
+
+print(nums)
